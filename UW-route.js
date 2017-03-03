3f8ed49fcbfd2c8f17fd9bb15f38178ed6aa0c68
Daniel Wang <s92025592025@gmail.com>
2017-03-02 23:00:23 -0800

    Add node.js for module

---
+++ UW-route.js
@@ -0,0 +1,21 @@
+// @Author: Daniel Wang (JIUN-CHENG WANG)
+// @Description: This module is mainly used to analysis the location relationship on campus
+// 2017/01/31
+
+/* For testing and reference purpose
+
+// constructor
+var UWRoute = function (name){
+	this.name = name;
+};
+
+
+// instance function
+UWRoute.prototype.testing1 = function (age){
+	console.log('Hi!, my name is ' + this.name + 'and I\'m ' +
+				age + ' years old');
+}
+
+// export
+module.exports = UWRoute;
+*/
