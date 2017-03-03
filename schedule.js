03a373f98f479e28226695a9debc93b0fe012ac9
Daniel Wang <s92025592025@gmail.com>
2017-03-02 23:21:40 -0800

    Cookie test json array

--- schedule.js
+++ schedule.js
@@ -1,7 +1,7 @@
 (function (){
 	'use strict';
 
-	Cookies.set('classes', {classes:"123"});
+	Cookies.set('classes', {classes:["JAPAN 213", "PHYS 122", "ENGL 282"]});
 
 	console.log(Cookies.get('classes'));
 })();
