497b6ad3e5555559f1b2c81c61bc8e1174e5e3ba
Daniel Wang <s92025592025@gmail.com>
2017-03-04 13:29:01 -0800

    try to pass array to build schedule

--- schedule.js
+++ schedule.js
@@ -5,7 +5,8 @@
 
 	// dummy for developing state
 	Cookies.set('classes', {classes:["JAPAN 213", "PHYS 122", "ENGL 282"]});
-	console.log(Cookies.getJSON('classes').classes);
+	var classes = Cookies.getJSON('classes');
+	console.log(classes.classes);
 	// get "any" possible schedule
 	var schedules = UWCourse.buildSchedule(Cookies.getJSON('classes').classes);
 	console.log(schedules);
