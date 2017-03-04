10789e25c34d3605e69c931634a35378e47a609c
Daniel Wang <s92025592025@gmail.com>
2017-03-04 14:40:23 -0800

    sort things out

--- schedule.js
+++ schedule.js
@@ -3,19 +3,16 @@
 
 	const UWCourse = require('./UW-classes.js');
 
+	// find available schedule
 	// dummy for developing state
 	Cookies.set('classes', {classes:["JAPAN 213", "PHYS 122", "ENGL 282"]});
 	var classes = Cookies.getJSON('classes');
-	console.log(classes.classes);
 	// get "any" possible schedule
 	var schedules = UWCourse.buildSchedule(classes.classes);
-	console.log(schedules);
 
 	window.onload = function (){
-		// start find available schedule
-
 		// read filters
-
+		for(var i = 0; i < document.querySelectorAll('.travel-option')){}
 		// display on block and calendar
 	};
 
