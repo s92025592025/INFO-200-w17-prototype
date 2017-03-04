4e011fda8dfe25a1f88f0943a2337f71b353a683
Daniel Wang <s92025592025@gmail.com>
2017-03-03 22:02:25 -0800

    test without browserfy" "

--- schedule.js
+++ schedule.js
@@ -1,7 +1,12 @@
 (function (){
 	'use strict';
 
+	const UWCourse = require('./UW-classes.js');
+
 	Cookies.set('classes', {classes:["JAPAN 213", "PHYS 122", "ENGL 282"]});
 
 	console.log(Cookies.get('classes'));
+
+	// test browserfy
+	console.log(UWCourse.getClassSections(Cookies.getJSON('classes').classes[0]));
 })();
