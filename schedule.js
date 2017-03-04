a7251dfc88a5b8de48806d4556f938f5c392f3c9
Daniel Wang <s92025592025@gmail.com>
2017-03-04 15:07:59 -0800

    bais structure to filter by travel time

--- schedule.js
+++ schedule.js
@@ -2,18 +2,38 @@
 	'use strict';
 
 	const UWCourse = require('./UW-classes.js');
+	const UWRoute = require('./UW-route.js')
 
 	// find available schedule
 	// dummy for developing state
 	Cookies.set('classes', {classes:["JAPAN 213", "PHYS 122", "ENGL 282"]});
 	var classes = Cookies.getJSON('classes');
 	// get "any" possible schedule
-	var schedules = UWCourse.buildSchedule(classes.classes);
+	const SCHEDULES = UWCourse.buildSchedule(classes.classes);
+	var filteredSchedule = [];
 
 	window.onload = function (){
 		// read filters
-		for(var i = 0; i < document.querySelectorAll('.travel-option')){}
+		for(var i = 0; i < document.querySelectorAll('.travel-option')){
+			document.querySelectorAll('.travel-option').onchange = validSchedule;
+		}
 		// display on block and calendar
 	};
+
+	function validSchedule (){
+		var passedSchedule = [];
+		var route = new UWRoute('AIzaSyBRJAizomD3x1U7FX2PZM7PEDxs_UQXFWQ');
+
+		if(this.value == mixed){
+			filteredSchedule = SCHEDULES;
+		}
+
+		for(var i = 0; i < SCHEDULES.length; i++){
+			// filter travek time
+		}
+
+		// will filter data later
+		filteredSchedule = SCHEDULES;
+	}
 
 })();
